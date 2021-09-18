59ec3c741ef36b4e6f21b22301c2724930d387cc
Luka Peric <lukaperickv@gmail.com>
2021-09-18 08:39:38 +0200

    Add_header_and_Home_page

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import "../assets/styles/pages/Home.scss"
+
+
+const Home = () => {
+
+    return (
+        <div className="home">
+            <div className="currency-container">
+                <div className="currency-info">
+                    <p>Name</p>
+                    <p>Last</p>
+                    <p>Change</p>
+                    <p>Change Percent</p>
+                    <p>High</p>
+                    <p>Low</p>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Home;
